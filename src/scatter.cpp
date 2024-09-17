7fcd1fb69e45eff27cbc3090e9f0e0f2e039ea8a
VedantSawant6900 <67890801+VedantSawant6900@users.noreply.github.com>
2024-09-17 19:45:01 -0400

    Scatter

--- src/scatter.cpp
+++ src/scatter.cpp
@@ -3,8 +3,6 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <ctime>
-#include <cstdlib>  // For rand() and srand()
 
 using namespace std;
 
@@ -18,12 +16,13 @@ void parsec_roi_end()
 
 }
 
+
 struct Result {
-    vector< vector<int> > A;
+	vector< vector<int> > A;
 };
 
 Result read(string filename) {
-	vector< vector<int> > A;
+	vector< vector<int> > A, B;
 	Result ab;
 	string line;
 	ifstream infile;
@@ -47,32 +46,50 @@ Result read(string filename) {
 }
 
 vector< vector<int> > scatter(vector< vector<int> > A) {
-    int n = A.size();
+    int n=A.size();
+    const int num_samples = 1000;
+    vector<int> x_indices(num_samples);
+    vector<int> y_indices(num_samples);
+    vector<float> data_samples(num_samples);
+	for (int i = 0; i < num_samples; ++i) {
+        x_indices[i] = rand() % n;
+        y_indices[i] = rand() % n; 
+        data_samples[i] = rand() % 100 + 1; 
+    }
 
-    // Generate 1,000 random indices and 1,000 random values
-    for (int i = 0; i < 1000; i++) {
-        int row = rand() % n;   // Random row index
-        int col = rand() % n;   // Random column index
-        int value = rand()%100;     // Random value without bounds
-
-        // Scatter the random value into the matrix
-        A[row][col] = value;
+    for (int i = 0; i < num_samples; ++i) {
+        int x = x_indices[i];
+        int y = y_indices[i];
+        A[x][y] += data_samples[i];
     }
     return A;
 }
 
-int main(int argc, char* argv[]) {
-    srand(time(0));
-    string filename;
-    if (argc < 3) {
-        filename = "2000.in";
-    } else {
-        filename = argv[2];
-    }
-    Result result = read(filename);
-    cout<<result.A;
+void printMatrix(vector< vector<int> > matrix) {
+	vector< vector<int> >::iterator it;
+	vector<int>::iterator inner;
+	for (it=matrix.begin(); it != matrix.end(); it++) {
+		for (inner = it->begin(); inner != it->end(); inner++) {
+			cout << *inner;
+			if(inner+1 != it->end()) {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main (int argc, char* argv[]) {
+	string filename;
+	if (argc < 3) {
+		filename = "2000.in";
+	} else {
+		filename = argv[2];
+	}
+	Result result = read (filename);
     parsec_roi_begin();
-    vector< vector<int> > C = scatter(result.A);
+	vector< vector<int> > C = scatter(result.A);
     parsec_roi_end();
-    return 0;
+	//printMatrix(C);
+	return 0;
 }
